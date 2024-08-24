47ebbe84f459d21bc9f7c71003793c92c2165033
aminwhat <m.aminwhat@gmail.com>
2024-08-24 22:43:09 +0330

    make changes to dart_frog sample

---
+++ samples/dart_frog/client/lib/core/config.dart
@@ -0,0 +1,5 @@
+typedef Config = ({
+  String baseUrl,
+});
+
+const Config config = (baseUrl: "http://192.168.0.200:8080");
--- samples/dart_frog/server/routes/hello_page.dart
+++ samples/dart_frog/server/routes/hello_page.dart
@@ -1,12 +1,17 @@
 import 'dart:async';
-
-import 'package:dart_frog/dart_frog.dart' hide Request, Handler;
+import 'dart:developer';
+import 'package:dart_frog/dart_frog.dart';
 import 'package:shelf_swap/shelf_swap.dart';
 
 FutureOr<Response> onRequest(RequestContext context) {
-  return widget(
-    (context) async => const HelloPage(),
-  )(context);
+  log('hello_page received');
+  if (context.request.method == HttpMethod.get) {
+    return widget(
+      (context) async => const HelloPage(),
+    )(context);
+  }
+
+  return Response(body: 'Response From Dart Frog!!!');
 }
 
 class HelloPage extends StatelessWidget {
@@ -14,6 +19,40 @@ class HelloPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final shelf = InheritedShelf.of(context);
+    final page =
+        int.tryParse(shelf.request.request.url.queryParameters['page'] ?? '1');
+    return switch (page) {
+      1 => const _Page1(),
+      2 => const _Page2(),
+      _ => const _Page3(),
+    };
+  }
+}
+
+class _Page1 extends StatelessWidget {
+  const _Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Page1');
+  }
+}
+
+class _Page2 extends StatelessWidget {
+  const _Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Page2');
+  }
+}
+
+class _Page3 extends StatelessWidget {
+  const _Page3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Page3');
   }
 }
